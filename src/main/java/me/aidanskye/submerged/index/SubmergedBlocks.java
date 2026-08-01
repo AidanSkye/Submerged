2ab48f2ee94627489b18fd183377b5a73c3c3874
the-cat-gs <leyduncan@gmail.com>
2026-08-01 00:13:55 -0400

    block :D

--- src/main/java/me/aidanskye/submerged/index/SubmergedBlocks.java
+++ src/main/java/me/aidanskye/submerged/index/SubmergedBlocks.java
@@ -10,6 +10,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 
 public class SubmergedBlocks {
 
@@ -19,6 +20,44 @@ public class SubmergedBlocks {
     public static Block yellowBalloonBlock = registerBlock("yellow_balloon", new BalloonBlock(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL).nonOpaque().breakInstantly().noCollision()));
     public static Block redAndYellowBalloonBlock = registerBlock("red_and_yellow_balloons", new BalloonBlock(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL).nonOpaque().breakInstantly().noCollision()));
     public static Block greenAndBlueBalloonBlock = registerBlock("green_and_blue_balloons", new BalloonBlock(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL).nonOpaque().breakInstantly().noCollision()));
+
+    public static final Block CROSS_GLASS = registerBlock( "cross_glass", new TransparentBlock(AbstractBlock.Settings.create().sounds(BlockSoundGroup.GLASS).nonOpaque().blockVision(Blocks::never).solidBlock(Blocks::never)));
+    public static final Block DARK_MED_FLOOR = registerBlock( "dark_med_floor", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.METAL)));
+    public static final Block DARK_WALLPAPER = registerBlock( "dark_wallpaper", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block DEC_BLUE_WALLPAPER = registerBlock( "dec_blue_wallpaper", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block DEC_BROWN_FLOOR = registerBlock( "dec_brown_floor", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block DEC_RED_FLOOR = registerBlock( "dec_red_floor", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block DEC_WOOD_FLOOR = registerBlock( "dec_wood_floor", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOD)));
+    public static final Block GRAY_WALLPAPER = registerBlock( "gray_wallpaper", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block GREEN_CAF_FLOOR = registerBlock( "green_caf_floor", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.TUFF)));
+    public static final Block HEAVILY_RUST_STEEL = registerBlock( "heavily_rust_steel", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.NETHERITE)));
+    public static final Block LIGHT_RUST_STEEL = registerBlock( "light_rust_steel", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.NETHERITE)));
+    public static final Block LIGHT_MED_FLOOR = registerBlock( "light_med_floor",new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.METAL)));
+    public static final Block LIGHT_PURPLE_WALLPAPER = registerBlock( "light_purple_wallpaper", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block LIGHT_WALLPAPER = registerBlock( "light_wallpaper", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block MED_WALL = registerBlock( "med_wall", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.METAL)));
+    public static final Block ORANGE_TILES = registerBlock( "orange_tiles", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.TUFF)));
+    public static final Block POLISHED_STEEL = registerBlock( "polished_steel", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.NETHERITE)));
+    public static final Block PURPLE_CAF_FLOOR = registerBlock( "purple_caf_floor", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.TUFF)));
+    public static final Block PURPLE_DEC_WALLPAPER = registerBlock( "purple_dec_wallpaper", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block PURPLE_WALLPAPER = registerBlock( "purple_wallpaper", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SEASTONE_PILLAR = registerBlock( "seastone_pillar", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.CALCITE)));
+    public static final Block SEASTONE_TILES = registerBlock( "seastone_tiles", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.CALCITE)));
+    public static final Block SMOOTH_STEEL = registerBlock( "smooth_steel", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.NETHERITE)));
+    public static final Block SUBMERGED_BLUE_CARPET = registerBlock( "submerged_blue_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SUBMERGED_CYAN_CARPET = registerBlock( "submerged_cyan_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SUBMERGED_DARK_SANDSTONE= registerBlock( "submerged_dark_sandstone", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE)));
+    public static final Block SUBMERGED_DARK_YELLOW_CARPET = registerBlock( "submerged_dark_yellow_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SUBMERGED_GREEN_CARPET = registerBlock( "submerged_green_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SUBMERGED_LIME_CARPET = registerBlock( "submerged_lime_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SUBMERGED_MAGENTA_CARPET = registerBlock("submerged_magenta_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SUBMERGED_PURPLE_CARPET = registerBlock( "submerged_purple_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SUBMERGED_RED_CARPET = registerBlock( "submerged_red_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block SUBMERGED_SAND = registerBlock( "submerged_sand", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.SAND)));
+    public static final Block SUBMERGED_SANDSTONE = registerBlock( "submerged_sandstone", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE)));
+    public static final Block SUBMERGED_STONE = registerBlock( "submerged_stone", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.DEEPSLATE)));
+    public static final Block SUBMERGED_YELLOW_CARPET = registerBlock( "submerged_yellow_carpet", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
+    public static final Block YELLOW_DEC_WALLPAPER = registerBlock( "yellow_dec_wallpaper", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOL)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
--- src/main/java/me/aidanskye/submerged/index/SubmergedItemGroups.java
+++ src/main/java/me/aidanskye/submerged/index/SubmergedItemGroups.java
@@ -21,6 +21,44 @@ public class SubmergedItemGroups {
                         entries.add(SubmergedBlocks.yellowBalloonBlock);
                         entries.add(SubmergedBlocks.redAndYellowBalloonBlock);
                         entries.add(SubmergedBlocks.greenAndBlueBalloonBlock);
+
+                        entries.add(SubmergedBlocks.CROSS_GLASS);
+                        entries.add(SubmergedBlocks.DARK_MED_FLOOR);
+                        entries.add(SubmergedBlocks.DARK_WALLPAPER);
+                        entries.add(SubmergedBlocks.DEC_BLUE_WALLPAPER);
+                        entries.add(SubmergedBlocks.DEC_BROWN_FLOOR);
+                        entries.add(SubmergedBlocks.DEC_RED_FLOOR);
+                        entries.add(SubmergedBlocks.DEC_WOOD_FLOOR);
+                        entries.add(SubmergedBlocks.GRAY_WALLPAPER);
+                        entries.add(SubmergedBlocks.GREEN_CAF_FLOOR);
+                        entries.add(SubmergedBlocks.HEAVILY_RUST_STEEL);
+                        entries.add(SubmergedBlocks.LIGHT_RUST_STEEL);
+                        entries.add(SubmergedBlocks.LIGHT_MED_FLOOR);
+                        entries.add(SubmergedBlocks.LIGHT_PURPLE_WALLPAPER);
+                        entries.add(SubmergedBlocks.LIGHT_WALLPAPER);
+                        entries.add(SubmergedBlocks.MED_WALL);
+                        entries.add(SubmergedBlocks.ORANGE_TILES);
+                        entries.add(SubmergedBlocks.POLISHED_STEEL);
+                        entries.add(SubmergedBlocks.PURPLE_CAF_FLOOR);
+                        entries.add(SubmergedBlocks.PURPLE_DEC_WALLPAPER);
+                        entries.add(SubmergedBlocks.PURPLE_WALLPAPER);
+                        entries.add(SubmergedBlocks.SEASTONE_PILLAR);
+                        entries.add(SubmergedBlocks.SEASTONE_TILES);
+                        entries.add(SubmergedBlocks.SMOOTH_STEEL);
+                        entries.add(SubmergedBlocks.SUBMERGED_BLUE_CARPET);
+                        entries.add(SubmergedBlocks.SUBMERGED_CYAN_CARPET);
+                        entries.add(SubmergedBlocks.SUBMERGED_DARK_SANDSTONE);
+                        entries.add(SubmergedBlocks.SUBMERGED_DARK_YELLOW_CARPET);
+                        entries.add(SubmergedBlocks.SUBMERGED_GREEN_CARPET);
+                        entries.add(SubmergedBlocks.SUBMERGED_LIME_CARPET);
+                        entries.add(SubmergedBlocks.SUBMERGED_MAGENTA_CARPET);
+                        entries.add(SubmergedBlocks.SUBMERGED_PURPLE_CARPET);
+                        entries.add(SubmergedBlocks.SUBMERGED_RED_CARPET);
+                        entries.add(SubmergedBlocks.SUBMERGED_SAND);
+                        entries.add(SubmergedBlocks.SUBMERGED_SANDSTONE);
+                        entries.add(SubmergedBlocks.SUBMERGED_STONE);
+                        entries.add(SubmergedBlocks.SUBMERGED_YELLOW_CARPET);
+                        entries.add(SubmergedBlocks.YELLOW_DEC_WALLPAPER);
                     }).build());
 
     public static void initialize() {}
